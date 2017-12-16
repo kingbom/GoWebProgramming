4022b0f316599c586f61a162dbf5ec7ecf2dc2d1
bombay <bomjaruwit@gmail.com>
2017-12-17 02:11:17 +0700

    html template and reponse data form server

--- main.go
+++ main.go
@@ -3,15 +3,22 @@
  import(
 	"fmt"
 	"net/http"
+	"html/template"
 	"github.com/gorilla/mux"
  )
 
+ type Product struct {
+	 Name string
+	 Price int
+ }
+
  func main(){
-	  router := mux.NewRouter();
+	  router := mux.NewRouter()
 	  router.HandleFunc("/", index)
 	  router.HandleFunc("/login", login)
 	  router.HandleFunc("/signup", signup)
 	  router.HandleFunc("/file", file)
+	  router.HandleFunc("/product", getProduct)
 	  http.ListenAndServe(":8080", router) 
  }
 
@@ -29,6 +36,12 @@ func signup(w http.ResponseWriter, req *http.Request){
 
 func file(w http.ResponseWriter, req *http.Request){
 	http.ServeFile(w, req, "file.txt")
+}
+
+func getProduct(w http.ResponseWriter, req *http.Request){
+	var templates = template.Must(template.ParseFiles("product.html"))
+	product := Product{"Iphone", 22000}
+	templates.ExecuteTemplate(w, "product.html", product)
 }
 
  func getUser(w http.ResponseWriter, req *http.Request){
