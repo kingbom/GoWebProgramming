93b8c6df5006200c64618dcee435008d09cc9487
bombay <bomjaruwit@gmail.com>
2017-12-17 01:26:50 +0700

    serve file html

--- main.go
+++ main.go
@@ -9,13 +9,22 @@
  func main(){
 	  router := mux.NewRouter();
 	  router.HandleFunc("/", index).Methods("GET")
-	  router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	  router.HandleFunc("/login", login).Methods("GET")
+	  router.HandleFunc("/signup", signup).Methods("GET")
 	  http.ListenAndServe(":8080", router) 
  }
 
  func index(w http.ResponseWriter, req *http.Request){
-	 fmt.Fprintf(w, "index")
+	 http.ServeFile(w, req, "index.html")
  }
+
+ func login(w http.ResponseWriter, req *http.Request){
+	http.ServeFile(w, req, "login.html")
+}
+
+func signup(w http.ResponseWriter, req *http.Request){
+	http.ServeFile(w, req, "signup.html")
+}
 
  func getUser(w http.ResponseWriter, req *http.Request){
 	params := mux.Vars(req)
